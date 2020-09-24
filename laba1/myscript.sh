7d5d7a6790a1d9546d4ba9a54419296e717d1978
Rinat <r1nat.2019@yandex.ru>
2020-09-25 01:00:00 +0300

    calc commit

--- laba1/myscript.sh
+++ laba1/myscript.sh
@@ -5,6 +5,7 @@ source search
 source reverse
 source strlen
 source colors
+source myexit
 
 case "$1" in
 "calc")
@@ -34,4 +35,16 @@ then strlen "$2"
 else echo "I can have only 2 arguments!" 1>&2
 fi
 ;;
+
+#./mysript.sh exit 3
+"exit")
+if (($# == 1))
+then echo "exit: 0"
+exit 0
+elif (($# == 2))
+then myexit "$2"
+else echo "I can have only 2 arguments!" 1>&2 
+fi
+;;
+
 esac
