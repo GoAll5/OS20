e1c2b40ecb368bde57ede8fb0ccd1f11fe785462
Rinat <r1nat.2019@yandex.ru>
2020-09-24 21:38:16 +0300

    add myscript+colors

--- laba1/myscript.sh
+++ laba1/myscript.sh
@@ -4,18 +4,34 @@ source calc
 source search
 source reverse
 source strlen
+source colors
 
 case "$1" in
 "calc")
-calc "$2" "$3" "$4"
+if (( $# == 4 ))
+then calc "$2" "$3" "$4"
+else echo "I can have only 4 arguments!" 1>&2
+fi
 ;;
+
 "search")
-search "$2" "$3"
+if (( $# == 3 ))
+then search "$2" "$3"
+else echo "I can have only 4 arguments!" 1>&2
+fi
 ;;
+
 "reverse")
-reverse "$2" "$3"
+if (( $# == 3 ))
+then reverse "$2" "$3"
+else echo "I can have only 4 arguments!" 1>&2
+fi
 ;;
+
 "strlen")
-strlen "$2"
+if (( $# == 2 ))
+then strlen "$2"
+else echo "I can have only 2 arguments!" 1>&2
+fi
 ;;
 esac
