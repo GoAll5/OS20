f2ad53880170c892e3ef260d4a98fc841b3d5c21
Rinat <r1nat.2019@yandex.ru>
2020-12-18 08:16:33 +0300

    3 laba 2,3

--- lab3/firstTask.sh
+++ lab3/firstTask.sh
@@ -2,6 +2,6 @@
 mkdir -p ~/test && 
 {
 	echo "Catalog test was created successfully" > ~/report
-	touch ~/test/$(time +'%d.%m.%y-%H:%M:%S')
+	touch ~/test/$(date +'%d.%m.%y-%H:%M:%S')
 }
-ping -c 1 www.net_nikogo.ru || echo "$(time +'%d.%m.%y-%H:%M:%S') Ping lose!" >> ~/report
+ping -c 1 www.net_nikogo.ru || echo "$(date +'%d.%m.%y-%H:%M:%S') Ping lose!" >> ~/report
